fe13cda379a469f1eccf870a548a1ea97a5e5032
hsanbsri <08hasantrade@gmail.com>
2023-06-12 20:47:12 +0700

    update layout

--- app/src/main/java/com/example/customview/dCoffee/api/ApiConfig.kt
+++ app/src/main/java/com/example/customview/dCoffee/api/ApiConfig.kt
@@ -14,7 +14,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://story-api.dicoding.dev/v1/")
+                .baseUrl("https://cloud-computing-eta.vercel.app/register")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
